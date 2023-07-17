0e1c0ed2a0e31454233ea993a404cb2bbf33b820
Craluminum2413 <beware.14b@gmail.com>
2023-07-17 21:13:26 +0300

    add creative inventory tab automatically

--- src/Core.cs
+++ src/Core.cs
@@ -1,4 +1,5 @@
 using Vintagestory.API.Common;
+using Vintagestory.API.Util;
 
 [assembly: ModInfo("Fancy Planters")]
 
@@ -12,6 +13,25 @@ namespace FancyPlanters
             api.RegisterBlockClass("Planter2x1", typeof(BlockPlantContainer2x1));
             api.RegisterBlockBehaviorClass("FancyPlanters.BbName", typeof(BlockBehaviorName));
             api.World.Logger.Event("started 'Fancy Planters' mod");
+        }
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            foreach (var obj in api.World.Collectibles)
+            {
+                if (obj?.Attributes?["plantContainable"]?["fplanter2x1Container"]?.Exists == true)
+                {
+                    AddCreativeInventoryTab(obj, "fpallowed");
+                }
+            }
+        }
+
+        public static void AddCreativeInventoryTab(CollectibleObject obj, string tabName)
+        {
+            if (obj.CreativeInventoryTabs != null && obj.CreativeInventoryTabs.Length != 0 && !string.IsNullOrEmpty(obj?.CreativeInventoryTabs?[0]))
+            {
+                obj.CreativeInventoryTabs = obj.CreativeInventoryTabs.Append(tabName);
+            }
         }
     }
 }
